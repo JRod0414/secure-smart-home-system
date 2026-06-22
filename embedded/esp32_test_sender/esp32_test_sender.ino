01ebb7454b7cc94644d0110c71d7477bfe41439e
Jeff Peltier <jared.peltier@uri.edu>
2026-06-22 10:57:02 -0400

    Add ESP32 test event sender

---
+++ embedded/esp32_test_sender/esp32_test_sender.ino
@@ -0,0 +1,58 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "WIFI Name";
+const char* password = "WIFI Pass";
+
+const char* serverUrl = "http://ipv4:3000/api/events";
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to Wi-Fi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("Wi-Fi connected");
+  Serial.print("ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+
+  WiFiClient client;
+  HTTPClient http;
+
+  Serial.println("Sending test sensor event...");
+
+  http.begin(client, serverUrl);
+  http.addHeader("Content-Type", "application/json");
+
+  String jsonEvent =
+    "{\"device_id\":\"esp32_1\","
+    "\"sensor_type\":\"door\","
+    "\"event\":\"open\"}";
+
+  int responseCode = http.POST(jsonEvent);
+
+  Serial.print("HTTP response code: ");
+  Serial.println(responseCode);
+
+  if (responseCode > 0) {
+    String responseBody = http.getString();
+
+    Serial.println("Server response:");
+    Serial.println(responseBody);
+  } else {
+    Serial.println("Request failed. Check IP address, backend, and firewall.");
+  }
+
+  http.end();
+}
+
+void loop() {
+  // Sends once each time the ESP32 starts or resets.
+}

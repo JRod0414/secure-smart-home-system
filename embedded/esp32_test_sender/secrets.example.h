521e22f41d92672b0ad40f3443c489efd44ea67e
Jared Peltier <jared.peltier@uri.edu>
2026-06-25 11:49:54 -0400

    Add ESP32 secrets template

---
+++ embedded/esp32_test_sender/secrets.example.h
@@ -0,0 +1,6 @@
+#pragma once
+
+const char* ssid = "YOUR_WIFI_NAME";
+const char* password = "YOUR_WIFI_PASSWORD";
+const char* serverUrl = "http://YOUR_SERVER_LAN_IP:3000/api/events";
+const char* deviceApiKey = "YOUR_DEVICE_API_KEY";

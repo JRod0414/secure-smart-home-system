07e211f0afee97bb3a64790996578efc7ea56db4
Jared Peltier <jared.peltier@uri.edu>
2026-07-25 12:37:46 -0400

    Clean up permissions middleware

--- backend/security/permissions.js
+++ backend/security/permissions.js
@@ -4,15 +4,13 @@ const rolePermissions = {
 };
 
 function hasPermission(role, permission) {
-  // look up permissions for role
   const permissions = rolePermissions[role];
-  // return true or false
-  return Object.prototype.hasOwnProperty.call(rolePermissions, role) && permissions.includes(permission);
+  return Object.prototype.hasOwnProperty.call(rolePermissions, role) 
+  && permissions.includes(permission);
 }
 
 function requirePermission(permission) {
   return function (req, res, next) {
-    // check req.user.role with hasPermission
     if (!hasPermission(req.user.role, permission)) {
       return res.status(403).json({ error: "Forbidden" });
     }
